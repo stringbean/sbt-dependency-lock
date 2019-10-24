e5ea6959820b4d470579be6e22b2372ad6652945
Michael Stringer <stringbean@users.noreply.github.com>
2019-10-24 09:36:51 +0100

    Cache file hashes

--- src/main/scala/software/purpledragon/sbt/lock/DependencyUtils.scala
+++ src/main/scala/software/purpledragon/sbt/lock/DependencyUtils.scala
@@ -4,22 +4,33 @@ import java.time.Instant
 
 import sbt._
 
+import scala.collection.{immutable, mutable}
+
 object DependencyUtils {
   def resolve(updateReport: UpdateReport, configs: Seq[ConfigRef]): DependencyLockFile = {
     val configurations = updateReport.configurations.filter(config => configs.contains(config.configuration))
 
+    val checksumCache = mutable.Map[File, String]()
+
     val configModules = configurations map { conf =>
-      conf.configuration.name -> (conf.modules map { mr =>
-        val arts = mr.artifacts.map(toResolvedArtifact)
-        ResolvedDependency(mr.module.organization, mr.module.name, mr.module.revision, arts)
-      })
+      conf.configuration.name -> conf.modules.map(toResolvedDependency(_, checksumCache))
     }
 
     DependencyLockFile(1, Instant.now(), configModules.toMap)
   }
 
-  private def toResolvedArtifact(artifact: (Artifact, File)): ResolvedArtifact = {
-    val checksum = Hash.toHex(Hash(artifact._2))
-    ResolvedArtifact(artifact._1.name, s"sha1:$checksum")
+  private def toResolvedDependency(
+      module: ModuleReport,
+      checksumCache: mutable.Map[File, String]): ResolvedDependency = {
+
+    val artifacts: immutable.Seq[ResolvedArtifact] = module.artifacts map {
+      case (artifact, file) =>
+        val hash = checksumCache.getOrElseUpdate(file, hashFile(file))
+        ResolvedArtifact(artifact.name, hash)
+    }
+
+    ResolvedDependency(module.module.organization, module.module.name, module.module.revision, artifacts)
   }
+
+  private def hashFile(file: File): String = s"sha1:${Hash.toHex(Hash(file))}"
 }
